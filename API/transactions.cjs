4716fa3d0d5b9cee9112ed8c0a743e0a12dbde64
guccibibl <118521595+guccibibl@users.noreply.github.com>
2022-11-22 23:27:23 +0800

    Update transactions.cjs

--- API/transactions.cjs
+++ API/transactions.cjs
@@ -14,8 +14,9 @@ class CoinBaseTransaction{
 class Transaction{
     // input: TxInput[], output: TxOutput[]
     constructor(input, output){
-
-    }
+        public id: string;
+        public input: TxInput[];
+        public output: TxOutput[];
 
 }
 
@@ -23,21 +24,37 @@ class TxInput{
 
     // tx_id: string, tx_index: , sig: string
     constructor(tx_id, tx_index , sig){
-
+    public tx_id: string;
+    public tx_index: number;
+    public sig: string;
     }
 }
 
 class TxOutput{
+    
+    public to: string;
+    public amount: number;
 
     // to: , amount:number
     constructor(to, amount){
-
+        this.to = to;
+        this.amount = amount;
     }
 }
 
 
 class UnspentTxOut {
+    public readonly tx_id: string;
+    public readonly tx_index: number;
+    public readonly to: string;
+    public readonly amount: number;
 
+    constructor(tx_id, tx_index, to, amount) {
+        this.tx_id = tx_id;
+        this.tx_index = tx_index;
+        this.to = to;
+        this.amount = amount;
+    }
 }
 
 exports.CoinBaseTransaction= CoinBaseTransaction;
